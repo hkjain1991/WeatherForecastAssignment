e30c84d791ad4af1b7a815171935abcb90c26399
hemeandra <hemeandrajain>
2024-02-21 14:20:39 +0530

    added test class

--- app/src/test/java/com/example/weatherapp/WeatherDetailsViewModelTest.kt
+++ app/src/test/java/com/example/weatherapp/WeatherDetailsViewModelTest.kt
@@ -1,4 +1,67 @@
 package com.example.weatherapp
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.weatherapp.model.response.WeatherListData
+import com.example.weatherapp.service.WeatherService
+import com.example.weatherapp.viewmodel.WeatherDetailsViewModel
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+
+/**
+ * Test for [WeatherDetailsViewModel]
+ */
 class WeatherDetailsViewModelTest {
+
+    private lateinit var viewModel: WeatherDetailsViewModel
+    private val dispatcher: TestDispatcher = UnconfinedTestDispatcher()
+
+    @get:Rule
+    val rule: TestRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(dispatcher)
+        viewModel = WeatherDetailsViewModel()
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `test to verify tht weather data has got success`() = runTest {
+        val mock = mockk<WeatherService>()
+
+        coEvery {
+            mock.getUpcomingWeatherData("305901")
+        } returns WeatherListData(emptyList())
+        viewModel.getWeatherDataAccordingToZipCode("305901", mock)
+
+        assertEquals(viewModel.weatherLiveData.value?.isEmpty(), true)
+    }
+
+    @Test
+    fun `test to verify tht weather data has null body`() = runTest {
+        val mock = mockk<WeatherService>()
+        coEvery {
+            mock.getUpcomingWeatherData("305901")
+        } returns null
+        viewModel.getWeatherDataAccordingToZipCode("305901", mock)
+
+        assertNull(viewModel.weatherLiveData.value)
+    }
 }
